f0fee7ee398f79d50070c71a6725e5bca39022b2
Maks <maxim.m.nsk@gmail.com>
2020-12-08 00:27:33 +0700

    Trigger migration for MariaDB

--- application/migrations/sql/004_create_uuid_trigger.sql
+++ application/migrations/sql/004_create_uuid_trigger.sql
@@ -1,9 +1,8 @@
 use xiag;
-DELIMITER ;;
-CREATE OR REPLACE TRIGGER `xiag_poll_uuid` BEFORE INSERT ON `poll`
+
+CREATE TRIGGER IF NOT EXISTS `xiag_poll_uuid` BEFORE INSERT ON `poll`
  FOR EACH ROW BEGIN
   IF new.uuid IS NULL OR new.uuid = '' THEN
     SET new.uuid = UUID();
   END IF;
-END;;
-DELIMITER ;
+END;
