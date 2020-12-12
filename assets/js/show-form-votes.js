7a5e76faadfdb3045bbea0e14db35e101900a2ae
Maks <maxim.m.nsk@gmail.com>
2020-12-12 19:02:35 +0700

    WS connection address

--- assets/js/show-form-votes.js
+++ assets/js/show-form-votes.js
@@ -3,7 +3,7 @@ export function WebSocketVote(){
   var log = false;
   let addr = $("#add-data").attr("wss-addr");
   if(addr!==undefined){
-    var socket = new WebSocket("ws://xiag.test:27800");
+    var socket = new WebSocket("ws://"+addr);
   }else{
     return false;
   }
